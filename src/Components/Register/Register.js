c472d023a25e7cc7dbf44c9571795d47130a56dc
BmRahman <bmrahman23@gmail.com>
2022-11-24 18:28:23 +0600

    firebase done

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [signupError, setSignupError] = useState('')
+
+    const handleRegister = data => {
+        console.log(data)
+        
+    }
+
+
     return (
-        <div>
-            
-        </div>
+        <div className='h-[600px] lg:h-[800px] flex justify-center items-center'>
+        <div className='w-96 p-7 shadow-2xl'>
+        <form onSubmit={handleSubmit(handleRegister)}>
+        <h2 className='text-center text-lg font-bold text-accent mb-6'>Sign Up</h2>
+        <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input type="text" {...register("name", {required: "*Your name is required!"})} className="input input-bordered w-full mb-3"/>
+            {errors.name && <p role="alert" className='text-red-400'>{errors.name?.message}</p>}
+            </div>
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input type="email" {...register("email", {required: "*Email Address is required!"})} className="input input-bordered w-full mb-3"/>
+            {errors.email && <p role="alert" className='text-red-400'>{errors.email?.message}</p>}
+            </div>
+            <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input type="password" {...register("password", {required: "*Please Enter A Valid Password!", minLength: {value: 6, message: "password must be 6 characters or longer"}})} className="input input-bordered w-full mb-3"/>
+            {errors.password && <p role="alert" className='text-red-400'>{errors.password?.message}</p>}
+            </div>
+            {/* <p>Forgot Password?</p> */}
+          {/* <p>{data}</p> */}
+          <button className='btn btn-accent w-full mt-3 mb-3' type='submit'>Sign Up</button>
+          
+            {
+              signupError && <p className='text-red-400'>{signupError}</p>
+            }
+          
+    </form>
+    <p className='text-xs text-center'>Already have an account? <Link to='/login' className='text-secondary'>Log in here</Link></p>
+    <div className="divider mt-4 mb-4">OR</div>
+    <button className='btn btn-accent btn-outline w-full mt-3 mb-3' type='submit'>Continue With Google</button>
+    </div>
+
+    </div>
     );
 };
 
---
+++ src/Contexts/AuthProvider.js
@@ -0,0 +1,60 @@
+import React, { createContext, useEffect, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth';
+import app from './../firebase/firebase.init';
+
+export const AuthContext = createContext() 
+const auth = getAuth(app)
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    // create user with email and password
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    // implement google sign in
+    const provider = new GoogleAuthProvider()
+
+    const googleLogin = () => {
+        setLoading(true)
+        return signInWithPopup(auth, provider)
+    }
+
+    // login user
+    const loginUser = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    // logout user
+    const logoutUser = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    // observer
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log(currentUser)
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () => unSubscribe()
+    }, [])
+
+
+
+    const authInfo = {user, createUser, googleLogin, loginUser, logoutUser, loading}
+    return (
+        <div>
+            <AuthContext.Provider value={authInfo}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    );
+};
+
+export default AuthProvider;
