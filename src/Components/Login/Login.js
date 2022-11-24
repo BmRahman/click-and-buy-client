8821c800ef63d8fdb9ed1850ea012e5cef6498b2
BmRahman <bmrahman23@gmail.com>
2022-11-24 22:26:59 +0600

    dropdown dashboard implepented

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { AuthContext } from './../../Contexts/AuthProvider';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const {loginUser, googleLogin} = useContext(AuthContext)
+    const [loginError, setLoginError] = useState('')
+
+    const navigate = useNavigate()
+
+    const handleLogin = data => {
+        console.log(data)
+        setLoginError('')
+        loginUser(data.email, data.password)
+        .then(res => {
+          const user = res.user;
+          console.log(user)
+          navigate('/')
+        })
+        .catch(error => {
+          console.error(error.message)
+          setLoginError(error.message)
+        })
+    }
+
+    const handleGoogle = () => {
+        googleLogin()
+        .then(() => {})
+        .catch(err => console.error(err))
+    }
+
     return (
-        <div>
-            
+        <div className='h-[600px] lg:h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7 shadow-2xl'>
+            <form onSubmit={handleSubmit(handleLogin)}>
+            <h2 className='text-center text-lg font-bold text-accent mb-6'>Login</h2>
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="email" {...register("email", {required: "*Email Address is required!"})} className="input input-bordered w-full mb-3"/>
+                {errors.email && <p role="alert" className='text-red-400'>{errors.email?.message}</p>}
+                </div>
+                <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="password" {...register("password", {required: "*Please Enter A Valid Password!", minLength: {value: 6, message: "password must be 6 characters or longer"}})} className="input input-bordered w-full mb-3"/>
+                {errors.password && <p role="alert" className='text-red-400'>{errors.password?.message}</p>}
+                </div>
+                <p>Forgot Password?</p>
+              {/* <p>{data}</p> */}
+              <button className='btn btn-accent w-full mt-3 mb-3' type='submit'>Login</button>
+              {
+                loginError && <p className='text-red-400 mb-3'>{'*wrong password! please try again with valid password'}</p>
+              }
+        </form>
+        <p className='text-xs text-center'>New to Doctor's Portal? <Link to='/register' className='text-secondary'>Create New Account</Link></p>
+        <div className="divider mt-4 mb-4">OR</div>
+        <button onClick={handleGoogle} className='btn btn-accent btn-outline w-full mt-3 mb-3' type='submit'>Continue With Google</button>
+        </div>
+
         </div>
     );
 };
--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../Contexts/AuthProvider';
 
 const Header = () => {
+    const {user, logoutUser} = useContext(AuthContext)
+
+    const handleSignOut = () => {
+      logoutUser()
+      .then(() => {})
+      .catch(err => console.error(err))
+    }
+
 
     const menuItems = 
                       <>
                       <li><Link to='/' className='text-lg hover:text-primary'>Home</Link></li>
                       <li><Link to='/blog' className='text-lg hover:text-primary'>Blog</Link></li>
-                      <li><Link to='/login' className='text-lg hover:text-primary'>Login</Link></li>
-                      <li><Link to='/register' className='text-lg hover:text-primary'>Sign Up</Link></li>
-                      </>
+                      {
+                        user?.email || user?.uid ? 
 
+                        <>
+                          <li>
+                            <Link>
+                              <p className='text-lg hover:text-primary'>Dashboard</p>
+                              <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                            </Link>
+                            <ul className="p-2 z-10 bg-accent">
+                              <li className='text-primary'><Link>Submenu 1</Link></li>
+                              <li className='text-primary'><Link>Submenu 2</Link></li>
+                            </ul>
+                          </li>
+                        <li><button onClick={handleSignOut} className='btn btn-outline btn-primary rounded-xl'>Log Out</button></li>
+                        </>
+                       
+                       :
+                       <>
+                       <li><Link to='/login' className='text-lg hover:text-primary'>Login</Link></li>
+                       <li><Link to='/register' className='text-lg hover:text-primary'>Sign Up</Link></li>
+                       </>
+                      }
+                      </>
+                      
     return (
         <div>
             <div className="navbar bg-base-100">
