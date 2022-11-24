a65bb79d3b1aceab676d2b51136406ccc086231b
BmRahman <bmrahman23@gmail.com>
2022-11-25 03:36:30 +0600

    admin role updated

--- src/Components/Dashboard/AllUsers/AllUsers.js
+++ src/Components/Dashboard/AllUsers/AllUsers.js
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { toast } from 'react-hot-toast';
 
 const AllUsers = () => {
-    const {data: users = []} = useQuery({
+    const {data: users = [], refetch} = useQuery({
         queryKey: ['users'],
         queryFn: async() => {
             const res = await fetch('http://localhost:5000/users')
@@ -10,6 +11,24 @@ const AllUsers = () => {
             return data
         }
     })
+
+    const handleMakeAdmin = id => {
+        fetch(`http://localhost:5000/users/admin/${id}`, {
+          method: 'PUT',
+          headers: {
+            authorization: `bearer ${localStorage.getItem('accessToken')}`
+          }
+        })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          if(data.modifiedCount > 0){
+            toast.success('admin added succesfully')
+            refetch()
+          }
+        })
+      }
+
     return (
         <div>
             <h2 className='text-center text-3xl font-bold mt-5 mb-10'>All Users</h2>
@@ -22,6 +41,7 @@ const AllUsers = () => {
                     <th></th>
                     <th>Name</th>
                     <th>Email</th>
+                    <th>Verify</th>
                     <th>Admin</th>
                     <th>Delete</th>
                   </tr>
@@ -33,7 +53,8 @@ const AllUsers = () => {
                     <th>{i+1}</th>
                     <td>{user.name}</td>
                     <td>{user.email}</td>
-                    <td>{<button className='btn btn-xs btn-primary'>Make Admin</button>}</td>
+                    <td><button className='btn btn-xs btn-secondary'>Verify</button></td>
+                    <td>{user?.role !== 'admin' && <button onClick={() => handleMakeAdmin(user._id)} className='btn btn-xs btn-primary'>Make Admin</button>}</td>
                     <td><button className='btn btn-xs btn-error'>Delete</button></td>
                   </tr>)
                   }
